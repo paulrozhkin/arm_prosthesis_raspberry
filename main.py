c5d6223a2a6285f0d19ab21a76f2ad0b333145ce
Eugene51-78 <46783600+Evgenesius@users.noreply.github.com>
2020-11-15 19:43:58 +0300

    gesture execution

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ import sys
 import os
 import platform
 import random
+from datetime import datetime
 from threading import Thread
+from queue import Queue
 #from bluedot.btcomm import BluetoothClient
 #from signal import pause
 #import spidev
 
+def save_gesture(gesture_list):
+    f = open('gesture_list', 'w')
+    f.write(gesture_list)
 
 def serial_ports():
     """ Lists serial port names
@@ -44,10 +49,40 @@ def serial_ports():
 def data_received(data):
     print(data)
 
-def signal_recognizer(value):
-    global signal
-    signal = value
+class Command(object):
+    def __init__(self, Name, Payload):
+        self.Name = Name
+        self.Payload = Payload
 
+class Gesture(object):
+    def __init__(self, ID, NAME, LastTimeSync,
+                 IterableGesture, NumberOfGestureRepetitions,
+                 NumberOfMotions, ListActions):
+        self.ID = ID
+        self.NAME = NAME
+        self.LastTimeSync = LastTimeSync
+        self.IterableGesture = IterableGesture
+        self.NumberOfGestureRepetitions = NumberOfGestureRepetitions
+        self.NumberOfMotions = NumberOfMotions
+        self.ListActions = ListActions
+
+class GestureAction(object):
+    def __init__(self, PointerFingerPosition, MiddleFingerPosition, RingFinderPosition,
+                 LittleFingerPosition, ThumbFingerPosition, Delay):
+        self.PointerFingerPosition = PointerFingerPosition
+        self.MiddleFingerPosition = MiddleFingerPosition
+        self.RingFinderPosition = RingFinderPosition
+        self.LittleFingerPosition = LittleFingerPosition
+        self.ThumbFingerPosition = ThumbFingerPosition
+        self.Delay = Delay
+
+def MQTT_Proxy_command_receive():
+    print("MQTT Proxy command was received")
+    command = Command("Telemetry", 100)
+    return command
+
+def MQTT_Proxy_telemetry_send():
+    print ("send telemetry to MQTT")
 
 def ADC_MOCK():
     return random.randint(0, 65535)
@@ -73,14 +108,14 @@ def SPI_TELEMETRY_REQUEST_MOCK():
     telemetry_request = [A, B, C, D, E, F, G, CRC8, I, J, K, L, M, N, O, P]
     return telemetry_request
 
-def SPI_SET_POSITION_REQUEST_MOCK():
+def SPI_SET_POSITION_REQUEST(action):
     A = 1
     B = random.randint(0, 65535)
-    PointerFingerPosition = random.randint(0, 65535)
-    MiddleFingerPosition = random.randint(0, 65535)
-    RingFinderPosition = random.randint(0, 65535)
-    LittleFingerPosition = random.randint(0, 65535)
-    ThumbFingerPosition = random.randint(0, 65535)
+    PointerFingerPosition = action.PointerFingerPosition    #random.randint(0, 65535)
+    MiddleFingerPosition = action.MiddleFingerPosition  #random.randint(0, 65535)
+    RingFinderPosition = action.RingFinderPosition  #random.randint(0, 65535)
+    LittleFingerPosition = action.LittleFingerPosition  #random.randint(0, 65535)
+    ThumbFingerPosition = action.ThumbFingerPosition    #random.randint(0, 65535)
     CRC8 = random.randint(0, 65535)
     I = random.randint(0, 65535)
     G = random.randint(0, 65535)
@@ -118,6 +153,7 @@ def SPI_SET_POSITION_ANSWER_MOCK():
     return answer
 
 def SPI_MOCK(spi_request):
+    print(spi_request[0])
     if spi_request[0] == 0:
         return SPI_TELEMETRY_REQUEST_MOCK()
     else:
@@ -125,15 +161,28 @@ def SPI_MOCK(spi_request):
 
 def recognize_adc(adc_value):
     global signal
-    signal = adc_value
+    signal = round(adc_value*3/65536, 2)
+    print("VOLT: "+str(signal))
+    #уведомить поток gesture executor (main)
+    #тут выбрать жест из памяти или ...
+    rec = random.randint(1, 100)
+    if rec > 50:
+        ListActions = []
+        Action1 = GestureAction(2, 4, 8, 16, 32, 0.5)
+        Action2 = GestureAction(128, 64, 32, 16, 8, 1)
+        ListActions.append(Action1)
+        ListActions.append(Action2)
+        gesture = Gesture(1, "Test Gesture", 0, 0, 1, 2, ListActions)
+        print("send gesture in the Queue of gestures")
+        gesture_queue.put(gesture)
 
 def send_to_display(adc_value):
-    time.sleep(adc_value/10000000000)
+    display_command = [adc_value, 0xFFFF, 0xFFFF]
+    print("display : ", display_command)
 
 def get_adc_value():
     global adc_value
     adc_value = ADC_MOCK()
-    recognize_adc(adc_value)
     print("ADC_VALUE: ", adc_value)
 
 def get_SPI_telemetry():
@@ -143,40 +192,163 @@ def get_SPI_telemetry():
     print("TELEMETRY: ", driver_state)
 
 def get_SPI_answer():
-    set_position_request = SPI_SET_POSITION_REQUEST_MOCK()
+    set_position_request = SPI_SET_POSITION_REQUEST(GestureAction(0,0,0,0,0,0))
     spi_answer = SPI_MOCK(set_position_request)
     print("SPI_ANSWER: ", spi_answer)
 
+def SPI_send_set_position_request(action):
+    request = SPI_SET_POSITION_REQUEST(action)
+    print("Set Pos Request: ", request)
+
+def send_telemetry_to_MQTT(driver_state):
+    print("send_telemetry_to_MQTT", driver_state)
+
+def MQTT_Proxy_send_answer(command_name):
+    print("send_reply_to_MQTT for command ", command_name)
+
+def send_settings_to_MQTT():
+    print ("send_settings_to_MQTT")
+
+def set_settings_from_MQTT(settings):
+    print("set_settings_from_MQTT")
+
+def send_gestures_list_to_MQTT():
+    #достать список и отправить
+    list = [current_gesture]
+    print("send_gestures_list_to_MQTT")
+
+def save_gesture_from_MQTT(gesture):
+    print("save_gesture_from_MQTT")
+
+def delete_gesture_from_MQTT(gesture):
+    print("delete_gesture_from_MQTT")
+
+def find_gesture_by_id(id):
+    print("find_gesture_by_id")
+
 class I2C_thread(Thread):
     def run(self):
+        display_time = time.time()
         while 1:
-            for i in range(1000):
-                get_adc_value()
-                time.sleep(0.0025)
-            send_to_display(adc_value)
+            get_adc_value()
+            time.sleep(0.005)
+            now = time.time()
+            if now - display_time > 0.15:
+                display_time = now
+                send_to_display(adc_value)
 
-class telemetry_thread(Thread):
+class SPI_telemetry_thread(Thread):
     def run(self):
         while 1:
             get_SPI_telemetry()
             time.sleep(0.017)
 
-class spi_answer_thread(Thread):
+class SPI_answer_thread(Thread):
     def run(self):
         while 1:
             get_SPI_answer()
             time.sleep(1)
 
+class adc_recognizer_thread(Thread):
+    def run(self):
+        while 1:
+            recognize_adc(adc_value)
+            time.sleep(0.005)
+
+class SPI_set_position_thread(Thread):
+    cur = Gesture
+    def run(self):
+        while 1:
+            cur_gest = set_position_queue.get()
+            # если есть запрос, то отправляем, иначе скипаем.
+            if cur_gest:
+                print("we are sending set position requests")
+                action_number = -1
+                repeat_counter = 0
+                #жест может повторяться... по какому событию он прекращает повторяться?
+                while cur_gest.IterableGesture or action_number < cur_gest.NumberOfMotions \
+                        and repeat_counter < cur_gest.NumberOfGestureRepetitions:
+                    action_number += 1
+                    action = cur_gest.ListActions[action_number]
+                    SPI_send_set_position_request(action)
+                    if (cur_gest.NumberOfMotions == action_number + 1):
+                        action_number = 0
+                        repeat_counter += 1
+                    print("Cur Delay ", action.Delay)
+                    time.sleep(action.Delay)
+                print("End of gesture execution")
+
+class MQTT_commands_executor_thread(Thread):
+    def run(self):
+        global frequency
+        frequency = 1
+        while 1:
+            #always send telemetry
+            send_telemetry_to_MQTT(driver_state)
+            time.sleep(1/frequency)
+            # sometimes exec commands
+            command = MQTT_command_queue.get()
+            if command:
+                if command.Name == "Telemetry":
+                    frequency = random.randint(1, 200)
+                elif command.Name == "GetSettings":
+                    send_settings_to_MQTT()
+                elif command.Name == "SetSettings":
+                    settings = command.Payload
+                    set_settings_from_MQTT(settings)
+                elif command.Name == "GetGestures":
+                    send_gestures_list_to_MQTT()
+                elif command.Name == "SaveGesture":
+                    gesture = command.Payload
+                    save_gesture_from_MQTT(gesture)
+                elif command.Name == "DeleteGesture":
+                    gesture = command.Payload
+                    delete_gesture_from_MQTT(gesture)
+                elif command.Name == "PerformGestureId":
+                    id = command.Payload
+                    gesture = find_gesture_by_id(id)
+                    # нужна приоритетная очередь для жестов?
+                    if gesture != None:
+                        gesture_queue.put(gesture)
+                elif command.Name == "PerformGestureRaw":
+                    gesture = command.Payload
+                    gesture_queue.put(gesture)
+
 def threaded():
     I2C_thread().start()
-    telemetry_thread().start()
-    spi_answer_thread().start()
+    adc_recognizer_thread().start()
+    SPI_telemetry_thread().start()
+    SPI_answer_thread().start()
+    SPI_set_position_thread().start()
+    MQTT_commands_executor_thread().start()
+
 # EC:56:23:F3:91:FC - Honor 10 Lite
 # 98:D3:71:F9:7A:02 - HCF97A02
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    set_position_queue = Queue()
+    gesture_queue = Queue()
+    MQTT_command_queue = Queue()
+
+    global current_gesture
+    global mode
+    mode = 2 #AUTO
+
     threaded()
+
+    while(1):
+        gesture = gesture_queue.get()
+        if gesture:
+            print("Have a gesture for execution")
+            set_position_queue.put(gesture)
+
+        command = MQTT_Proxy_command_receive()
+        if command:
+            MQTT_command_queue.put(command)
+
+        #MQTT_Proxy_send_answer(command.name)
+
     #main()
 
     # nearby_devices = bluetooth.discover_devices(lookup_names=True)
@@ -184,6 +356,7 @@ if __name__ == '__main__':
     #
     # for addr, name in nearby_devices:
     #     print("  {} - {}".format(addr, name))
+
 '''
     # Передача на MQTT Proxy
     c = BluetoothClient("98:D3:71:F9:7A:02", data_received)
